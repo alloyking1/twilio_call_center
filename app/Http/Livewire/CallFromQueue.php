165e50b7605436455f0e58c083e45792c66e29ee
alloyking1 <alloyking1@gmail.com>
2023-04-17 16:03:02 +0100

    added a livewire function to route call

--- app/Http/Livewire/CallFromQueue.php
+++ app/Http/Livewire/CallFromQueue.php
@@ -3,9 +3,30 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use Twilio\Rest\Client;
 
 class CallFromQueue extends Component
 {
+    public function Call()
+    {
+        $connect = $this->connect();
+        $connect->calls->create(
+            "+12762965123",
+            "+2348063146940",
+            ["url" => "https://handler.twilio.com/twiml/EH2ff0457fe3b6eb226e48a3091d00e8c5"]
+        );
+
+        // print $call->sid;
+    }
+
+    public function connect()
+    {
+        $sid = getenv("TWILIO_ACCOUNT_SID");
+        $token = getenv("TWILIO_AUTH_TOKEN");
+        $twilio = new Client($sid, $token);
+        return $twilio;
+    }
+
     public function render()
     {
         return view('livewire.call-from-queue');
--- resources/views/livewire/call-from-queue.blade.php
+++ resources/views/livewire/call-from-queue.blade.php
@@ -1,3 +1,3 @@
 <div>
-    {{-- Success is as dangerous as failure. --}}
+    <button class="bg-white text-blue-500 p-3 rounded-md border border-blue-500" wire:click="Call">Connect to que</button>
 </div>
